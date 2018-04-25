cf9ec7cf3f042555aaa41888aa11b32777337801
rao <pramod_rao@outlook.com>
2018-04-25 19:36:20 +0530

    removed imshow, modifed main
    Signed-off-by: rao <pramod_rao@outlook.com>

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 """
 train and test CIFAR10 with pytorch
-""
+"""
+import os
+
+import torch
+import torchvision
+from torchvision import datasets, transforms
+
+from models import *
+from lib import utils, data_loader, parse_config
+
+args = parse_config.parser.parse_args()
+
+
