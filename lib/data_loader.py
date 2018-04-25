d55121055c24b0128c8e81f723c99a8c5629c90f
Daimler Pramod Rao <pramod.rao@daimler.com>
2018-04-25 15:41:08 +0530

    modified scratch pad
    Signed-off-by: Daimler Pramod Rao <pramod.rao@daimler.com>

--- lib/data_loader.py
+++ lib/data_loader.py
@@ -63,16 +63,17 @@ def create_class():
 
 
 #### scratch pad ####
+if __name__ == "__main__":
+    trfrm = create_tr_te_transfrom()
+    _data_set,_tr,_te = create_tr_te_data()
 
-trfrm = create_tr_te_transfrom()
-_data_set,_tr,_te = create_tr_te_data()
+    #print(_tr.train_data.mean(axis=(0,1,2))/255)
+    #print(_tr.train_data.std(axis=(0,1,2))/255)
 
-#print(_tr.train_data.mean(axis=(0,1,2))/255)
-#print(_tr.train_data.std(axis=(0,1,2))/255)
+    #mean = [0.49139968 0.48215841 0.44653091]
+    #std = [0.24703223 0.24348513 0.26158784]
 
-#mean = [0.49139968 0.48215841 0.44653091]
-#std = [0.24703223 0.24348513 0.26158784]
-
+"""
 # get some random training images
 dataiter = iter(_data_set["train"])
 images, labels = dataiter.next()
@@ -83,7 +84,6 @@ imshow(torchvision.utils.make_grid(images))
 print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 # without using pytorch built-in package
-"""
 data_folder = "data/"
 def _get_file_path(filename=""):
     return os.path.join(data_folder,"cifar-10-batches-py",filename)
